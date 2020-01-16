ce2ff066f1b4bb291c8e0d9e0c06100981e0d118
AleksakhinAS <freeman05@yandex.ru>
2020-01-16 13:03:30 +0300

    v2

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 process.env.NTBA_FIX_319 = 1;
 
-const TelegramBot = require('node-telegram-bot-api');
+var TelegramBot = require('node-telegram-bot-api');
 
-const token = '981150414:AAEQ6jfdeV5_4pXdIoV2dZibbFHJs59kMEc';
+var token = '981150414:AAEQ6jfdeV5_4pXdIoV2dZibbFHJs59kMEc';
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+var bot = new TelegramBot(token, {polling: true});
 
 bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
+  var chatId = msg.chat.id;
 
   // send a message to the chat acknowledging receipt of their message
   bot.sendMessage(chatId, 'Received your message');
